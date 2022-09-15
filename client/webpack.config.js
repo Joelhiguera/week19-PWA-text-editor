baec5458aa3353b75041ed62c602f430e0c08dc4
Joel Higuera <Joelhiguera10@gmail.com>
2022-09-15 16:03:16 -0600

    webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -69,5 +69,19 @@ module.exports = () => {
           },
       ],
     },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          include: path.resolve(__dirname, 'src'),
+          exclude: /(node_modules|bower_components|build)/,
+          loader: 'babel-loader'
+        },
+        {
+          test: /\.(css|less)$/,
+          use: ["style-loader", "css-loader"]
+        }
+      ]
+    },
   };
 };
